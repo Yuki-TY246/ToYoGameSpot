b61fb8e038232e7f3a5a8100657a54ccbff969f5
kogawa301 <kogawa0301toyou@gmail.com>
2024-06-25 09:08:26 +0900

    コードと色の修正

--- games/othello2.js
+++ games/othello2.js
@@ -133,7 +133,7 @@ function highlightLegalMoves1(ishi) {
                 masu.remove();
                 if (count > 0) {
                     // ハイライトクラスを適用し、背景色を変える
-                    $('#r' + r + 'c' + c + ' div').addClass('highlight').parent().css('background-color', '#538955');
+                    $('#r' + r + 'c' + c + ' div').addClass('highlight').parent().css('background-color', '#638955');
                 }
             }
         }
@@ -152,7 +152,7 @@ function highlightLegalMoves2(ishi) {
                 masu.remove();
                 if (count > 0) {
                     // ハイライトクラスを適用し、背景色を変える
-                    $('#r' + r + 'c' + c + ' div').addClass('highlight').parent().css('background-color', '#538955');
+                    $('#r' + r + 'c' + c + ' div').addClass('highlight').parent().css('background-color', '#638955');
                 }
             }
         }
